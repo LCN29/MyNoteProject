6f26bce1824d4300a93d0a5e9dbe2acb7d24c2f1
LCN29 <licanxin233@163.com>
2018-04-10 09:10:23 +0800

    logback

---
+++ slf4j-logback/src/main/java/com/eigpay/Main.java
@@ -0,0 +1,24 @@
+package com.eigpay;
+
+import com.eigpay.appender.Appender;
+
+/**
+ * Description:
+ *
+ * @Author LCN
+ * @Date 2018-04-08 下午 06:59
+ */
+public class Main {
+
+    public static void main(String[] args) {
+    /*    MyLogger logger = new MyLogger();
+        logger.showLog();*/
+
+/*        NoDebug noDebug = new NoDebug();
+        noDebug.showLog();*/
+
+        Appender logger = new Appender();
+        logger.showLog();
+    }
+
+}
